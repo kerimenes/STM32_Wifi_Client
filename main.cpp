75ba46356d166b7a8ea8b2bdfab92b9e5fd19893
kerimenes <kerimenes@bilkon-kontrol.com.tr>
2018-05-02 14:42:39 +0300

    DISCO_L475VG: WifiClient: Code fixed.

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ wifi_dataready pin (PE_1), wifi reset pin (PE_8)
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
 Serial pc(SERIAL_TX, SERIAL_RX);
-uint8_t RemoteIP[] = {MBED_CONF_APP_SERVER_IP_1,MBED_CONF_APP_SERVER_IP_2,MBED_CONF_APP_SERVER_IP_3, MBED_CONF_APP_SERVER_IP_4};
+uint8_t RemoteIP[] = {192, 168, 2, 5};
 uint8_t RxData [500];
 char* modulename;
 uint8_t TxData[] = "STM32 : Hello!\n";
@@ -29,86 +29,111 @@ uint16_t RxLen;
 uint8_t  MAC_Addr[6]; 
 uint8_t  IP_Addr[4]; 
 
+
+uint16_t myPort = 8002;
+int32_t socket = -1;
+
+int initWifi(void);
+int openWifiClientConnection(uint8_t *ipaddr, uint16_t port);
+
 int main()
 {
-    int32_t Socket = -1;
     uint16_t Datalen;
     uint16_t Trials = CONNECTION_TRIAL_MAX;
 
     pc.baud(115200);
 
     printf("\n");
-    printf("************************************************************\n");
-    printf("***   STM32 IoT Discovery kit for STM32L475 MCU          ***\n");
-    printf("***      WIFI Module in TCP Client mode demonstration    ***\n\n");
-    printf("*** TCP Client Instructions :\n");
-    printf("*** 1- Make sure your Phone is connected to the same network that\n");
-    printf("***    you configured using the Configuration Access Point.\n");
-    printf("*** 2- Create a server by using the android application TCP Server\n");
-    printf("***    with port(8002).\n");
-    printf("*** 3- Get the Network Name or IP Address of your phone from the step 2.\n\n"); 
+    printf("Starting...");
     printf("************************************************************\n");
 
     /*Initialize  WIFI module */
+    int ret = initWifi();
+    if (ret != 0) {
+        printf("Closing\r\n");
+        return -1;
+    }
+
+    ret = openWifiClientConnection(RemoteIP, myPort);
+    if (ret != 0) {
+        printf("Closing\r\n");
+        return -1;
+    }
+
+    while(1){
+        if(socket != -1) {
+            if(WIFI_ReceiveData(socket, RxData, sizeof(RxData), &Datalen, WIFI_READ_TIMEOUT) == WIFI_STATUS_OK){
+                if(Datalen > 0) {
+                    if(WIFI_SendData(socket, TxData, sizeof(TxData), &Datalen, WIFI_WRITE_TIMEOUT) != WIFI_STATUS_OK) {
+                        printf("> ERROR : Failed to send Data.\n");
+                    } 
+                }
+            } else {
+                printf("> ERROR : Failed to Receive Data.\n");
+            }
+        }
+    }
+}
+
+int initWifi(void)
+{
+    int ret = 0;
     if(WIFI_Init() ==  WIFI_STATUS_OK) {
-        printf("> WIFI Module Initialized.\n");  
+        printf("> WIFI Module Initialized.\n");
         if(WIFI_GetMAC_Address(MAC_Addr) == WIFI_STATUS_OK) {
-            printf("> es-wifi module MAC Address : %X:%X:%X:%X:%X:%X\n",     
+            printf("> es-wifi module MAC Address : %X:%X:%X:%X:%X:%X\n",
                    MAC_Addr[0],
                    MAC_Addr[1],
                    MAC_Addr[2],
                    MAC_Addr[3],
                    MAC_Addr[4],
-                   MAC_Addr[5]);   
+                   MAC_Addr[5]);
         } else {
             printf("> ERROR : CANNOT get MAC address\n");
+            ret = -1;
         }
-    
-        if( WIFI_Connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, WIFI_ECN_WPA2_PSK) == WIFI_STATUS_OK) {
+
+        if( WIFI_Connect("Mickey2018", "mickey2018", WIFI_ECN_WPA2_PSK) == WIFI_STATUS_OK) {
             printf("> es-wifi module connected \n");
             if(WIFI_GetIP_Address(IP_Addr) == WIFI_STATUS_OK) {
-                printf("> es-wifi module got IP Address : %d.%d.%d.%d\n",     
+                printf("> es-wifi module got IP Address : %d.%d.%d.%d\n",
                        IP_Addr[0],
                        IP_Addr[1],
                        IP_Addr[2],
-                       IP_Addr[3]); 
-        
-                printf("> Trying to connect to Server: %d.%d.%d.%d:8002 ...\n",     
-                       RemoteIP[0],
-                       RemoteIP[1],
-                       RemoteIP[2],
-                       RemoteIP[3]);
-        
-                while (Trials--){ 
-                    if( WIFI_OpenClientConnection(0, WIFI_TCP_PROTOCOL, "TCP_CLIENT", RemoteIP, 8002, 0) == WIFI_STATUS_OK){
-                        printf("> TCP Connection opened successfully.\n"); 
-                        Socket = 0;
-                    }
-                }
-                if(!Trials) {
-                    printf("> ERROR : Cannot open Connection\n");
-                }
+                       IP_Addr[3]);
             } else {
                 printf("> ERROR : es-wifi module CANNOT get IP address\n");
+                ret = -2;
             }
         } else {
             printf("> ERROR : es-wifi module NOT connected\n");
+            ret = -3;
         }
     } else {
-        printf("> ERROR : WIFI Module cannot be initialized.\n"); 
+        printf("> ERROR : WIFI Module cannot be initialized.\n");
+        ret = -4;
     }
-  
-    while(1){
-        if(Socket != -1) {
-            if(WIFI_ReceiveData(Socket, RxData, sizeof(RxData), &Datalen, WIFI_READ_TIMEOUT) == WIFI_STATUS_OK){
-                if(Datalen > 0) {
-                    if(WIFI_SendData(Socket, TxData, sizeof(TxData), &Datalen, WIFI_WRITE_TIMEOUT) != WIFI_STATUS_OK) {
-                        printf("> ERROR : Failed to send Data.\n");   
-                    } 
-                }
-            } else {
-                printf("> ERROR : Failed to Receive Data.\n");  
-            }
+    return ret;
+}
+
+int openWifiClientConnection(uint8_t *ipaddr, uint16_t port)
+{
+    int ret = 0;
+    printf("> Trying to connect to Server: %d.%d.%d.%d:%d ...\n",
+           ipaddr[0],
+           ipaddr[1],
+           ipaddr[2],
+           ipaddr[3],
+            myPort);
+    uint16_t Trials = CONNECTION_TRIAL_MAX;
+    while(Trials--) {
+        if (WIFI_OpenClientConnection(0, WIFI_TCP_PROTOCOL, "TCP_CLIENT",ipaddr, port, 0) == WIFI_STATUS_OK) {
+            printf("> TCP Connection opened successfully.\n");
+            socket = 0;
+            ret = 0;
         }
     }
+    if (Trials == 0)
+        ret = -1;
+    return ret;
 }
